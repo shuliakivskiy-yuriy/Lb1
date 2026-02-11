fbceaeace319fc7c019b8a0ef7e4403765cb6e44
Шуляківський Юрій Сергійович <ipzk251_shyus@student.ztu.edu.ua>
2026-02-11 18:02:47 +0200

    Refactor: extracted discount logic to OrderService

---
+++ Kursova/Services/OrderService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursova.Services
+{
+    public class OrderService
+    {
+        public decimal CalculateDiscount(decimal totalAmount)
+        {
+            if (totalAmount >= 50000) return 10.0m;
+            if (totalAmount >= 10000) return 5.0m;
+            return 0.0m;
+        }
+
+        public decimal CalculateFinalSum(decimal totalAmount)
+        {
+            decimal discount = CalculateDiscount(totalAmount);
+            return totalAmount - (totalAmount * discount / 100);
+        }
+    }
+}
